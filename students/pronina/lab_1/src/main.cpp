4e6a226dbf74dc7e34e6f84a9eef473970fd93e6
Asya Pronina <155jj@mail.ru>
2016-05-20 12:05:38 +0300

    Refactoring is done. Short concept is implemented.

--- students/pronina/lab_1/src/main.cpp
+++ students/pronina/lab_1/src/main.cpp
@@ -4,41 +4,20 @@
 using namespace std;
 using namespace cv;
 
-template <int thread>
-char*& dist_window_name()
-{
-	static char* window_name;
-	return window_name;
-}
-
-template <int thread>
-char*& smooth_window_name()
-{
-	static char* window_name;
-	return window_name;
-}
-
-template <int thread>
-char*& path_to_image()
-{
-	static char* path;
-	return path;
-}
-
-template<int thread>
 void Smoothing(int parameter, void* params)
 {
+	int threshold = ((int **)params)[0][0], kernel_divider = ((int **)params)[0][1];
+	char *path_to_image = ((char ***)params)[1][0],\
+		*dist_window_name = ((char ***)params)[1][1], *smooth_window_name = ((char ***)params)[1][2];
 
-	int threshold = ((int *)params)[0];
-	int kernel_divider = ((int *)params)[1];
-	Mat src = imread(path_to_image<thread>(), CV_LOAD_IMAGE_COLOR), \
+	Mat src = imread(path_to_image, CV_LOAD_IMAGE_COLOR), \
 		contours, distances, sum, convolutional(src.size(), CV_8UC3);
 
 	Canny(src, contours, threshold, threshold * 3);
 	contours = 255 - contours;
 
 	distanceTransform(contours, distances, CV_DIST_L2, CV_DIST_MASK_PRECISE);
-	imshow(dist_window_name<thread>(), distances);
+	imshow(dist_window_name, distances);
 
 	integral(src, sum, CV_8UC1);
 
@@ -59,26 +38,26 @@ void Smoothing(int parameter, void* params)
 		}
 	}
 
-	imshow(smooth_window_name<thread>(), convolutional);
+	imshow(smooth_window_name, convolutional);
 }
 
 int main(int argc, char** argv)
 {
 	if (argc >= 2)
 	{
-		const int thread = 0;
-		path_to_image<thread>() = argv[1];
-		dist_window_name<thread>() = "Distances";
-		smooth_window_name<thread>() = "Smoothing";
+		char* dist_window_name = "Distance";
+		char* smooth_window_name = "Smoothing";
+		namedWindow(dist_window_name);
+		namedWindow(smooth_window_name);
 
-		namedWindow(dist_window_name<thread>());
-		namedWindow(smooth_window_name<thread>());
+		int int_params[] = {0, 0};
+		char* string_params[] = {argv[1], dist_window_name, smooth_window_name};
+		void* params[] = {int_params, string_params};
 
-		int params[] = {0, 0};
-		createTrackbar( "Threshold:", dist_window_name<thread>(), &params[0], 100, Smoothing<thread>, params);
-		createTrackbar( "Kernel divider:", smooth_window_name<thread>(), &params[1], 20, Smoothing<thread>, params);
+		createTrackbar("Threshold:", dist_window_name, &int_params[0], 100, Smoothing, params);
+		createTrackbar("Kernel divider:", smooth_window_name, &int_params[1], 20, Smoothing, params);
 
-		Smoothing<thread>(0, params);
+		Smoothing(0, params);
 		waitKey(0);
 	}
 	else 
